4c6f7280f7c74c28ab9d0a547cb55e98f32d9f54
Tavac75 <74030355+Tavac75@users.noreply.github.com>
2022-03-24 19:12:19 -0500

    Complete

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 -- Part 1: Test it with SQL
-
+    select * from job;
 -- Part 2: Test it with SQL
-
+    select name from employer where location="St. Louis City";
 -- Part 3: Test it with SQL
-
+    drop table job;
 -- Part 4: Test it with SQL
+    select name from skill inner join job_skills on skill.id=job_skills.skills_id order by name asc;
